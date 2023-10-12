894a1d8d0fe8ddbf7c0bb5bc4768b57dc0e9c638
dzhengiz.turhan@outlook.com <dzhengiz.turhan@outlook.com>
2023-10-12 14:31:10 +0300

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServiceExtensions(builder.Configuration);   
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
